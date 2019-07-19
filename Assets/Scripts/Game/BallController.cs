0a75cbf251620ba11904724cbafb4ec72f9005c4
Bertum <albertodelcru@gmail.com>
2019-07-19 23:48:07 +0200

    Ball controller

---
+++ Assets/Scripts/Game/BallController.cs
@@ -0,0 +1,52 @@
+﻿using UnityEngine;
+
+public class BallController : MonoBehaviour
+{
+    private Rigidbody rigidbody;
+    private float force;
+    public float MaxForce = 50;
+    public bool canMove = false;
+
+    private void Awake()
+    {
+        rigidbody = this.GetComponent<Rigidbody>();
+    }
+
+    private void Start()
+    {
+        canMove = true;
+    }
+
+    void Update()
+    {
+        if (canMove)
+        {
+            if (Input.GetMouseButton(0))
+            {
+                force += Time.deltaTime;
+            }
+
+            if (Input.GetMouseButtonUp(0))
+            {
+                canMove = false;
+                PushBall(Mathf.Clamp(force, 0, MaxForce));
+                force = 0;
+            }
+        }
+    }
+
+    private void PushBall(float forceToApply)
+    {
+        rigidbody.AddForce(transform.forward * forceToApply);
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.tag == "Pin")
+        {
+            //Activate timer and check if pins are moving??
+        }
+    }
+
+
+}
---
+++ Assets/Scripts/Game/GameController.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+
+public class GameController : MonoBehaviour
+{
+    private bool firstPlayerTurn;
+    private float scorePlayerOne, scorePlayerTwo;
+
+    void Start()
+    {
+        scorePlayerOne = 0;
+        scorePlayerTwo = 0;
+        firstPlayerTurn = true;
+    }
+
+    private void ChangeTurn()
+    {
+        firstPlayerTurn = !firstPlayerTurn;
+    }
+}
