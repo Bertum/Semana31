3411f45aa0b5d274a8bd1a6b152af55cb9d5df48
Bertum <albertodelcru@gmail.com>
2019-07-20 12:58:44 +0200

    Game UI

--- Assets/Scripts/Game/GameController.cs
+++ Assets/Scripts/Game/GameController.cs
@@ -1,9 +1,19 @@
 ﻿using UnityEngine;
+using UnityEngine.UI;
 
 public class GameController : MonoBehaviour
 {
     private bool firstPlayerTurn;
     private float scorePlayerOne, scorePlayerTwo;
+    private BallController ballController;
+    private Text txtScorePlayerOne, txtScorePlayerTwo;
+
+    private void Awake()
+    {
+        ballController = GameObject.FindGameObjectWithTag("Ball").GetComponent<BallController>();
+        txtScorePlayerOne = GameObject.Find("GameUI/PanelPlayerOne/Score").GetComponent<Text>();
+        txtScorePlayerTwo = GameObject.Find("GameUI/PanelPlayerTwo/Score").GetComponent<Text>();
+    }
 
     void Start()
     {
@@ -15,5 +25,25 @@ public class GameController : MonoBehaviour
     private void ChangeTurn()
     {
         firstPlayerTurn = !firstPlayerTurn;
+        ballController.canMove = true;
+    }
+
+    public void SetPoints(float points)
+    {
+        if (firstPlayerTurn)
+        {
+            scorePlayerOne += points;
+        }
+        else
+        {
+            scorePlayerTwo += points;
+        }
+        UpdateUI();
+    }
+
+    private void UpdateUI()
+    {
+        txtScorePlayerOne.text = scorePlayerOne.ToString();
+        txtScorePlayerTwo.text = scorePlayerTwo.ToString();
     }
 }
